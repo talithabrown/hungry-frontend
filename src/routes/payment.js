d4bd23de404d85c646b0096606a0693c663ebf3e
Talitha Brown <talithabrown128@gmail.com>
2022-12-05 09:40:53 -0700

    fixed payment errors

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -11,6 +11,10 @@ const Payment = () => {
     const [alertType, setAlertType] = useState([ 'hideAlert' ])
     const [alertMessage, setAlertMessage] = useState([ '' ])
 
+    const [displayClassName, setDisplayClassName] = useState('payment-form')
+    const [displaySuccessClassName, setDisplaySuccessClassName] = useState('displayNone')
+
+
     const placeOrder = async () => {
         console.log('inside place order function')
 
@@ -42,7 +46,9 @@ const Payment = () => {
     }
 
     const navigateToOrderSuccess = () => {
-        navigate('/order-successful')
+        //navigate('/order-successful')
+        setDisplayClassName('displayNone')
+        setDisplaySuccessClassName('show')
     }
 
 
@@ -56,32 +62,33 @@ const Payment = () => {
           <main className="login-main">
           <Link to='/delivery-options' className="post-detail-back-link"><img src="/images/chevron-left.svg" alt="back icon"/></Link>
             <Alert message={alertMessage} type={alertType} closeAlert={closeAlert}/>
-            <form>
+            <h3 className={displaySuccessClassName}>Your order has been placed successfully!</h3>
+            <div className={displayClassName}>
                 <div>
                     <h2>Payment</h2>
  
-                    <label htmlFor="cname">Name on Card</label>
-                    <input required type="text" id="cname" name="cardname" placeholder="John More Doe" />
-                    <label htmlFor="ccnum">Credit card number</label>
-                    <input required type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444" />
-                    <label htmlFor="expmonth">Exp Month</label>
-                    <input required type="text" id="expmonth" name="expmonth" placeholder="September"></input>
+                    <label htmlFor="name">Name on Card</label>
+                    <input required type="text" id="name" name="name"/>
+                    <label htmlFor="num">Credit card number</label>
+                    <input required type="text" id="num" name="number" />
+                    <label htmlFor="exp">Exp Month</label>
+                    <input required type="text" id="exp" name="exp"></input>
 
                     <div>
                         <div>
-                            <label htmlFor="expyear">Exp Year</label>
-                            <input required type="text" id="expyear" name="expyear" placeholder="2018" />
+                            <label htmlFor="expy">Exp Year</label>
+                            <input required type="text" id="expy" name="expy" />
                         </div>
                         <div>
-                            <label htmlFor="cvv">CVV</label>
-                            <input required type="text" id="cvv" name="cvv" placeholder="352" />
+                            <label htmlFor="cv">CVV</label>
+                            <input required type="text" id="cv" name="cv"/>
                         </div>
                     </div>
                 </div>
 
                 {/* <input onClick={placeOrder} type="submit" value="Checkout" className="payment-checkout-btn" /> */}
                 <button onClick={placeOrder} className="payment-checkout-btn">Place Order</button>
-            </form>
+            </div>
           </main>
           <Footer />
       </div>
